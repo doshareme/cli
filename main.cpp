7e60c6ac5063deba8c97b74d9d0186775d1db560
Ved Nig <vednig12@gmail.com>
2024-05-06 23:36:03 +0530

    More Changes to Updated Upload Method and Setup Version Count

--- main.cpp
+++ main.cpp
@@ -2,15 +2,29 @@
 #include <cstdlib>
 
 int main(int argc, char* argv[]) {
-  if (argc < 3) {
-    std::cerr << "Usage: " << argv[0] << " <file_path> <server_url>" << std::endl;
+  if (argc < 2) {
+    std::cerr << "Usage: " << argv[0] << " <file_path> **<server_url>" << std::endl;
     return 1;
   }
-
-  std::string command = "curl -sfdTo /dev/null ";
+  
+  // if (argv[1]=='-h' || argv[1]=='--help') {
+  //   std::cout << "Usage: " << argv[0] << " <file_path> **<server_url>" << std::endl;
+  //   return 0;
+  // }
+  // if (argv[1]=='-v' || argv[1]=='--version') {
+  //   std::cout << "Version: 1.0" << std::endl;
+  //   return 0;
+  // }
+  std::string command = "curl -sF 'file=@";
   command += argv[1];
-  command += " ";
-  command += argv[2];
+  command += "'";
+  if (argc > 3){
+    command += " ";
+    command += argv[3];
+  } else{
+    command += " ";
+    command += "https://upload.doshare.me/upload";
+  }
   command += " -o result.json";
   std::cout << "Executing: " << command << std::endl;
 
@@ -22,6 +36,7 @@ int main(int argc, char* argv[]) {
     std::cerr << "curl command failed with code: " << result << std::endl;
     return 1;
   }
-  std::cout << "File upload successful!" <<  << std::endl;
+  std::cout << "File upload successful!" << std::endl;
   return 0;
+
 }
